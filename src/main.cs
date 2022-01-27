2876f68b78f739ad3841f45a9e2dc65d0a63c453
chinempc <chinem.pc@gmail.com>
2022-01-26 20:52:34 -0500

    .

---
+++ src/main.cs
@@ -0,0 +1,13 @@
+using System; // Basic utility classes and primitive types
+using System.Collections.Generic; // Lists, collections, etc..
+using System.Linq;                  // Data
+using System.Text;                  // Text and encoding
+using System.Threading.Tasks;       // Multithreading 
+
+namespace StudentServiceDatabase {
+    class main {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
