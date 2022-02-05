901c51637ff02ab33fc6eddf32db3285cabf2cad
NFT Chuck <55151579+chinempc@users.noreply.github.com>
2022-02-05 14:59:48 -0500

    Corrected file name

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;                  
 
 namespace StudentServiceDatabase {
-    class main {
+    class Program {
         static void Main(string[] args) {
         }
     }
