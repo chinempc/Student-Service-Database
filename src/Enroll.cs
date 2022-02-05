58b6d824fb066731192c06f67461da266f65b894
NFT Chuck <55151579+chinempc@users.noreply.github.com>
2022-02-05 14:42:01 -0500

    Setters and Getters

--- src/Enroll.cs
+++ src/Enroll.cs
@@ -5,7 +5,7 @@ using System.Runtime.dll
 namespace StudentServiceDatabase {
     public class Enroll {
 	private DateTime DateCreated;
-        private int Grade { get; set;}
+        private int Grade;
         private Student EnrolledStudent;
         private Courses EnrolledCourse;
         public Enroll(Student stu, Courses cours){
@@ -13,5 +13,21 @@ namespace StudentServiceDatabase {
 	    EnrolledCourse = cours;
 	    DateCreated = DateTime.Now;
         }
+	
+	public void SetGrade(int _grade) {
+	    Grade = _grade;
+	}
+	
+	public int GetGrade() {
+	    return Grade;
+	}
+	
+	public GetStudent() {
+	    return EnrolledStudent;
+	}
+	
+	public GetCourse() {
+	    return EnrolledCourse;
+	}
     }
 }
