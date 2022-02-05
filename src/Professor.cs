974b64b419a77fb892866b067cb0ecb14f3a5c43
chinempc <chinem.pc@gmail.com>
2022-02-05 14:43:38 -0500

    finished person, student, and professor classes

--- src/Professor.cs
+++ src/Professor.cs
@@ -6,32 +6,90 @@ using System.Threading.Tasks;
 
 namespace StudentServiceDatabase
 {
-    class Professor
+    class Professor : Person
     {
         private double salary;
-        //private List<Courses> teaching;
+        private List<Courses> teaching = new List<Courses>();
         private bool ReceivedRaise;
+        private MaxCourses = 6;
 
-        Professor(double sal)
+        Professor(double sal, string fname, string lname, DateTime dateB, string phoneNum, Address ad) : base(fname, lname, dateB, phoneNum, ad)
         {
             salary = sal;
         }
 
         public void Eligible4Bonus()
         {
-            ;
+            if (ReceivedRaise) {
+                Console.WriteLine($"{this.FirstName} has already recieved their salary boost...");
+                return;
+            } 
+            // Check if they are teaching more than 4 courses
+            if (teaching.Count > 4) {
+                salary += 20000;
+                RecievedRaise = true;
+            }
         }
-        public void AddCourse()
+
+        public void AddCourse(Courses CourseToAdd)
         {
-            ;
+            // Account for duplicates
+            if (teaching.Contains(CourseToAdd)) {
+                Console.WriteLine("This course is already being taught by this professor.\nCourse was not added...")
+                return;
+            }
+            teaching.Add(CourseToAdd);
         }
-        public void RemoveCourse()
-        {
-            ;
+
+        public void AddCourses(Courses CoursesToAdd) {
+            if(teaching.Count < MaxCourses & CoursesToAdd.Length <= MaxCourses - teaching.Count) {
+                foreach (var i in CoursesToAdd) {
+                    // account for duplicates
+                    if (teaching.Contains(i)) {
+                        Console.WriteLine(i.CourseCode + " is already being taught by this professor.\nCourse was not added...")
+                        continue;
+                    }
+                    teaching.Add(i);
+                }
+                Console.WriteLine("New Courses has been added! There are " + MaxCourses - teaching.Count + " Course available to be taught by professor " + this.FirstName + ".\n");
+            } else if (CoursesToAdd.Length > MaxCourses - teaching.Count) {
+                Console.WriteLine("Number of Courses passed in exceeds the number of Courses allowed to be taught professor " + this.FirstName + " by " + CoursesToAdd.Length - MaxCourses - teaching.Count + ".\n");
+            } else {
+                Console.WriteLine("Max number of Courses to be taught by " + this.FirstName + "have been reached! Cannot add new Course.\n");
+            }
         }
-        public void ChangeStudentGrade()
+
+        public void RemoveCourse(Courses CourseToRemove)
         {
-            ;
+            if (!teaching.Remove(CourseToRemove)) {
+                Console.WriteLine("This course does not exist in this professor's list of courses...");
+            } else {
+                Console.WriteLine("Course has been removed...");
+            }
+        }
+
+        public vod RemoveCourses(Courses CoursesToRemove) {
+            foreach (var i in CoursesToRemove) {
+                teaching.Remove(i);
+            }
+        }
+
+        public void ChangeStudentGrade(Student stud, Course StudentCourse int ChangedGrade)
+        {
+            // Assume that all the information passed in is correct
+            for (int i = 0; i<teaching.Count; i++) {
+                // find the index that matches the course
+                if (teaching[i] == StudentCourse) {
+                    // Find the index that matches the student
+                    for (int j = 0; j<teaching[i].enrollees.Count; j++) {
+                        if (teaching[i].enrollees[j].GetStudent() == stud) {
+                            teaching[i].enrollees[j].SetGrade(ChangedGrade);
+                        }
+                    }
+                }
+
+            }
+            
         }
     }
 }
--- src/Student.cs
+++ src/Student.cs
@@ -6,35 +6,53 @@ using System.Threading.Tasks;
 
 namespace StudentServiceDatabase
 {
-    class Student
+    class Student : Person
     {
         private bool IsInternation;
-        //private List<Enroll> enrolles;
+        private List<Enroll> EnrolledIn = new List<Enroll>();
 
-        Student(bool inter)
-        {
-            IsInternation = inter;
-        }
+        Student(bool inte = False, string fname, string lname, DateTime dateB, string phoneNum, Address ad) : base(fname, lname, dateB, phoneNum, ad)
+        {}
 
         public bool IsPartTime()
         {
-            return false;
+            return enrollees.Count <= 3;
         }
+
         public bool OnProbation()
         {
-            return false;
+	    int total = 0;
+            foreach(var i in EnrolledIn) {
+	    	total += i.Grade;
+	    }
+	    return total/EnrolledIn.Count < 60;
         }
-        public void AddCourse()
-        {
 
-        }
-        public void RemoveCourse()
+        public void AddCourse(CourseToEnroll)
         {
+	    if(this.enrollees.Count < 6) {
+	    	EnrolledIn.Add(CourseToEnroll)
+	    } else {
+	    	Console.WriteLine("Enroll failed; {0} has reached the max amount of coursese they can enroll in.", this.FirstName);
+	    }
+        }
 
-        }
-        public void ReportCard(int grade)
+        public void RemoveCourse(CourseToRemove)
         {
-            ;
+	    if(!EnrolledIn.Remove(CourseToRemove)) {
+	    	Console.WriteLine("Course does not exist...");
+	    } else {
+	    	Console.WriteLine("Course has been removed...");
+	    }
+        }
+
+        public void ReportCard()
+        {
+	    Console.WriteLine("{0}'s Report Card\n-------------------------");
+            foreach(var i in EnrolledIn) {
+	    	Console.WriteLine("{0} -> {1}\n", i.EnrolledCourse.CourseCode, i.Grade);
+	    }
+	    Console.WriteLine("-------------------------");
         }
     }
 }
